e424b1140d45e36479a894f5b04c5e67dccf49e7
Robson M. Gomes <robson.mesquita56@gmail.com>
2020-07-17 16:52:17 -0300

    State

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 class App extends React.Component {
+    state = {
+        name: 'Rob',
+        age: 21
+    };
     render() {
         return (
             <div className="app-content">
                 <h1>Hello, World!</h1>
-                <p>{ Math.random() * 10 }</p>
+                <p>My name is {this.state.name} and I am {this.state.age}</p>
             </div>
         );
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
